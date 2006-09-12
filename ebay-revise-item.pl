6677ef01bec3d9a925f12fbcaecd98f9d2b0f237
Igor Chudov <ichudov@algebra.com>
2006-09-12 13:33:27 +0000

    Import of ICHUDOV/Net-eBay-0.38 from CPAN.
    gitpan-cpan-distribution: Net-eBay
    gitpan-cpan-version:      0.38
    gitpan-cpan-path:         ICHUDOV/Net-eBay-0.38.tar.gz
    gitpan-cpan-author:       ICHUDOV
    gitpan-cpan-maturity:     released

---
+++ ebay-revise-item.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+
+use Net::eBay;
+use Data::Dumper;
+
+my $title = undef;
+my $subtitle = undef;
+my $price = undef;
+my $quantity = undef;
+my $category = undef;
+my $bin = undef;
+my $item = `cat item.txt`; chomp $item;
+
+my $use_descr = 1;
+
+my $done = 1;
+
+sub get_argument {
+  my ($name,$ref) = @_;
+  if( $ARGV[0] eq "--$name" ) {
+    shift @ARGV;
+    $$ref = shift @ARGV;
+    die "--$name requires an argument!" unless defined $$ref;
+    return 1;
+  }
+  return undef;
+}
+
+while( $done ) {
+  $done = 0;
+  next if $done = get_argument( 'title', \$title );
+  next if $done = get_argument( 'subtitle', \$subtitle );
+  next if $done = get_argument( 'price', \$price );
+  next if $done = get_argument( 'quantity', \$quantity );
+  next if $done = get_argument( 'bin', \$bin );
+  next if $done = get_argument( 'category', \$category );
+
+  if( $done = get_argument( 'item', \$item ) ) {
+    $use_descr = undef;
+    next;
+  }
+}
+
+die "invalid itemid '$item'" unless $item =~ /^\d+$/;
+
+my $request = {
+               Item => {
+                        ItemID => $item,
+                       },
+              };
+
+
+
+$request->{Item}->{Title}         = $title if ( $title ); 
+$request->{Item}->{SubTitle}      = $subtitle if ( $subtitle ); 
+$request->{Item}->{StartPrice}    = $price if ( $price ); 
+$request->{Item}->{Quantity}      = $quantity if ( $quantity ); 
+$request->{Item}->{BuyItNowPrice} = $bin if ( $bin ); 
+$request->{Item}->{PrimaryCategory} = $category if ( $category ); 
+
+if( $use_descr ) {
+  die 'no file index.html' unless -f 'index.html';
+  my $descr = `cat index.html`;
+  $request->{Item}->{Description} = "<![CDATA[ $descr ]]>";
+}
+
+my $ebay = new Net::eBay;
+
+my $result = $ebay->submitRequest( 'ReviseItem', $request );
+
+if( ref $result ) {
+  #print "Succeeded\n" . Dumper( $result ) . "\n\n";
+  print "Succeeded!\n\n";
+
+  my $total = 0;
+  foreach my $fee (@{$result->{Fees}->{Fee}}) {
+    my $amount = $fee->{Fee}->{content};
+    
+    next unless $amount > 0 && $fee->{Name} ne 'ListingFee';
+    
+    print "Fee: $fee->{Name}: $amount.\n";
+    $total += $amount;
+  }
+  print "-----------------------------
+TOTAL FEE: $total
+
+";
+
+} else {
+  print "Failed: " . Dumper( $result ) . "\n";
+}
--- lib/Net/eBay.pm
+++ lib/Net/eBay.pm
@@ -21,11 +21,11 @@ Net::eBay - Perl Interface to XML based eBay API.
 
 =head1 VERSION
 
-Version 0.37
+Version 0.38
 
 =cut
 
-our $VERSION = '0.37';
+our $VERSION = '0.38';
 
 =head1 SYNOPSIS
 
